825d2c2ce594df8c86b7df2f8e05076f20b4d7ee
mohasal0101 <mohasal0101@gmail.com>
2022-09-07 10:42:01 +0300

    fixed-validation

---
+++ error-handlers/500.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (err, req, res, next) => {
+  if (err) {
+    res.status(500).send({
+      code: 500,
+      message: `Server Error: ${err.message || err}`});
+  } else {
+    next();
+  }
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const errorHandler = require('./error-handlers/500');
+const validateNumber = require('./middlewares/validate-number');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/square', validateNumber(), (req, res) => {
+  const number = req.squareNum;
+  res.status(200).json({ num: number });
+});
+
+app.use(errorHandler);
+
+/* istanbul ignore next */
+function start(port) {
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+}
+
+module.exports = {
+  start: start,
+  app: app,
+};
